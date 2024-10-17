2a4b49118a7ecf7594a385444e08a1773a6f5b43
LorenzoPranio <lorenzopranio02@gmail.com>
2024-10-17 16:23:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppWelcome from './components/AppWelcome.vue'
+</script>
+
+<template>
+  <AppHeader  />
+  <AppWelcome />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppWelcome.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+}
+</script>
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>Welcome Travelers...</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
